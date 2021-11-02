e6bcd772de847e0d7a2d97dd08f994cc2ca79173
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2021-11-02 16:12:56 +0100

    Pujar a git

---
+++ Escenes/ControlTempsProva.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTempsProva : MonoBehaviour
+{
+    [ContextMenu("Prova1")]
+    void Prova1()
+    {
+        ControlTemps.Temps(0, 1f);
+    }
+    [ContextMenu("Prova2")]
+    void Prova2()
+    {
+        ControlTemps.Temps(1, 0.5f);
+    }
+    [ContextMenu("Prova3")]
+    void Prova3()
+    {
+        ControlTemps.Temps(1);
+    }
+    [ContextMenu("Prova4")]
+    void Prova4()
+    {
+        ControlTemps.Pausar(2);
+    }
+}
---
+++ Scripts/ControlTemps.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XS_Utils;
+
+//[CreateAssetMenu(menuName = "Xido Studio/ControlTemps", fileName = "ControlTemps")]
+public static class ControlTemps
+{
+    /*public static ControlTemps Instance;
+
+    [HideInInspector] public GameObject prefab;
+    public static GameObject Prefab
+    {
+        get
+        {
+            return ControlTemps.Instance.prefab;
+        }
+    }*/
+
+
+    /*private void Awake()
+    {
+        Instance = this;
+    }
+    private void OnEnable()
+    {
+        Instance = this;
+    }*/
+    
+    static float objectiu;
+    static float velocitat;
+    public static void Temps(float _objectiu, float _velocitat)
+    {
+        objectiu = _objectiu;
+        velocitat = _velocitat;
+
+        Corrutina.While(ProperAObjectiu, CanviarTemps);
+        /*CrearGestor(objectiu);
+        if (ControlTemps_Gestor.Instance == null)
+            return;
+
+        ControlTemps_Gestor.Instance.Temps(objectiu, velocitat);*/
+    }
+    static float Distancia => Mathf.Clamp01(Mathf.Abs(objectiu - Time.timeScale));
+    static bool ProperAObjectiu() => Distancia < 0.02f;
+    static void CanviarTemps() 
+    {
+        if (!ProperAObjectiu())
+            Time.timeScale += Time.unscaledDeltaTime * (objectiu > Time.timeScale ? 1 : -1) * Distancia * velocitat;
+        else Time.timeScale = objectiu;
+    } 
+    public static void Temps(float objectiu)
+    {
+        Time.timeScale = objectiu;
+        /*CrearGestor(objectiu);
+        if (ControlTemps_Gestor.Instance == null)
+            return;
+
+        ControlTemps_Gestor.Instance.Temps(objectiu);*/
+    }
+    public static void Pausar()
+    {
+        Time.timeScale = 0;
+    }
+    public static void Pausar(float tempsPausat)
+    {
+        Time.timeScale = 0;
+        Corrutina.Iniciar(tempsPausat, () => Time.timeScale = 1);
+        /*CrearGestor(-1);
+        if (ControlTemps_Gestor.Instance == null)
+            return;
+
+        ControlTemps_Gestor.Instance.Pausar(temps);*/
+    }
+
+    /*static void CrearGestor(float objectiu)
+    {
+        if (objectiu == Time.timeScale)
+            return;
+
+        if(ControlTemps_Gestor.Instance == null)
+        {
+            Instantiate(Prefab).GetComponent<ControlTemps_Gestor>();
+        }
+    }
+
+    
+    static void CanviarTemps(float objectiu, float velocitat)
+    {
+        distancia = Mathf.Clamp01(Mathf.Abs(objectiu - Time.timeScale));
+        if (distancia > 0.02f)
+        {
+            Time.timeScale += Time.unscaledDeltaTime * (objectiu > Time.timeScale ? 1 : -1) * distancia * velocitat;
+        }
+    }
+    static void CanviarTemps(float objectiu)
+    {
+        Time.timeScale = objectiu;
+    }*/
+}
